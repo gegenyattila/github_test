fd70555f8325e917ec439155a54f66ff0b030ff9
gegenyattila <117274733+gegenyattila@users.noreply.github.com>
2022-11-02 12:23:24 +0100

    job prop added

--- github_test/github_commit_test.cs
+++ github_test/github_commit_test.cs
@@ -8,13 +8,15 @@ namespace github_test
 {
     public class github_commit_test
     {
-        public github_commit_test(string nev, int age)
+        public github_commit_test(string nev, int age, string job)
         {
             this.nev = nev;
             this.age = age;
+            this.job = job
         }
 
         public string nev { get; set; }
         public int age { get; set; }
+        public string job {get; set;}
     }
 }
