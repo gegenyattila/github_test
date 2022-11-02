e76004754499c9c5f7d97241fdef5f802e5ea1e7
gegeny.attila <ggggaba@gmail.com>
2022-11-02 12:20:04 +0100

    test class created

---
+++ github_test/github_commit_test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github_test
+{
+    public class github_commit_test
+    {
+        public github_commit_test(string nev, int age)
+        {
+            this.nev = nev;
+            this.age = age;
+        }
+
+        public string nev { get; set; }
+        public int age { get; set; }
+    }
+}
